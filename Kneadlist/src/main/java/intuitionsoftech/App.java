ab0cff313abea150e402ff298afbc00951e6f6a0
laksh <laksh@LAPTOP-PPEIE3H2>
2018-06-29 14:11:12 +1000

    This is the very first commit of Kneadlist project to the GitHub KneadlistRepo project

---
+++ Kneadlist/src/main/java/intuitionsoftech/App.java
@@ -0,0 +1,13 @@
+package intuitionsoftech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Kneadlist/src/test/java/intuitionsoftech/DemoTest.java
@@ -0,0 +1,12 @@
+package intuitionsoftech;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void show()
+	{
+		System.out.println("hello");
+	}
+}
